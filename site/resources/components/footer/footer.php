232ab154064e1a7f565d01e6aed9b06584c20da6
ConnorRMIT <s4096467@student.rmit.edu.au>
2025-06-21 00:09:41 +1000

    Feature: Added header and footer components

---
+++ site/resources/components/footer/footer.php
@@ -0,0 +1,13 @@
+<?php
+    class Footer{
+        function __construct()
+        {
+            echo("
+            <footer>
+                <p><a href=\"./\">Search Page</a></p>
+                <p><a href=\"./\">Add Items</a></p>
+            </footer>
+            ");
+        }
+    }
+?>
---
+++ site/resources/components/header/header.php
@@ -0,0 +1,18 @@
+<?php
+    class Header{
+        function __construct(string $title){
+            if(strlen($title)>0){
+                // I have to load the <svg> in here or else it complicates the hover colour when using <img>
+                $svg_in = file_get_contents("./resources/images/ui/hamburger.svg");
+
+                echo("<header>
+                    {$svg_in}
+                    <h1><a href=\"./\">{$title}</a></h1>
+                </header>");
+            }
+            else{
+                echo("Title is unset");
+            }
+        }
+    }
+?>
