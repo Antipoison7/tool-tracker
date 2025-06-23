128c6bc7929615b91e93557b604637e44f9e2596
ConnorRMIT <s4096467@student.rmit.edu.au>
2025-06-23 23:08:19 +1000

    Feature: Added and styled inputs for search page

--- site/index.php
+++ site/index.php
@@ -8,11 +8,27 @@
 <html>
     <head>
         <?php new Meta("Site Inventory Search", "A page that allows for searching the locations of various tools and parts", true); ?>
+        <link rel="stylesheet" href="./resources/styles/pages/index.css">
     </head>
     <body>
         <?php new Header("Search Page") ?>
         <main>
-            <p>Body Contents</p>
+            <div class="input-box">
+                <div class="search-box">
+                    <label for="search-bar">Search Item</label>
+                    <input type="text" name="search-bar" id="search-bar">
+                </div>
+    
+                <!-- Radio button for item selection -->
+                 <div class="radio-box">
+                     <input type="radio" id="name" name="search-type" checked="checked">
+                     <label for="name">Item Name</label>
+                     <input type="radio" id="category" name="search-type">
+                     <label for="category">Item Category</label>
+                     <input type="radio" id="group" name="search-type">
+                     <label for="group">Item Group</label>
+                 </div>
+            </div>
         </main>
         <?php new Footer() ?>
     </body>
