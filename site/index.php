69223e7d44632ad2094f1fec6e0676959aa9f349
ConnorRMIT <s4096467@student.rmit.edu.au>
2025-06-24 17:15:29 +1000

    Feature: Created sample item card

--- site/index.php
+++ site/index.php
@@ -27,7 +27,23 @@
                      <label for="category">Item Category</label>
                      <input type="radio" id="group" name="search-type">
                      <label for="group">Item Group</label>
+                     <input type="radio" id="storage" name="search-type">
+                     <label for="storage">Storage Location</label>
                  </div>
+            </div>
+            <div class="returned-data">
+                <div class="item-container">
+                    <div class="left">
+                        <h1>Hammer</h1>
+                        <h2><span class="highlight">Location:</span> Side Cupboard near the end of the garage</h2>
+                        <h3><span class="highlight">Position:</span> Top shelf on the left side</h3>
+                        <h4>Details</h4>
+                        <p>Hammer stolen off of google images, I am pretty sure it was listed on amazon at some point. The handle is made of plastic with more rubber on the end to allow it to grip better. Also the head is bent, I am not sure why they made it this way but it is really hard to hit nails with the thin side</p>
+                    </div>
+                    <div class="right">
+                        <img src="./resources/images/uploaded/hammer.png" alt="Hammer Image">
+                    </div>
+                </div>
             </div>
         </main>
         <?php new Footer() ?>
