82d7a13c59b66cb4f49057f6874f1becc4cf2bb8
ConnorRMIT <s4096467@student.rmit.edu.au>
2025-06-24 20:10:04 +1000

    Feature: Implemented the navigation bar

--- site/resources/components/header/header.php
+++ site/resources/components/header/header.php
@@ -3,12 +3,28 @@
         function __construct(string $title){
             if(strlen($title)>0){
                 // I have to load the <svg> in here or else it complicates the hover colour when using <img>
-                $svg_in = file_get_contents("./resources/images/ui/hamburger.svg");
+                $open_svg_in = file_get_contents("./resources/images/ui/hamburger.svg");
+                $close_svg_in = file_get_contents("./resources/images/ui/close.svg");
 
+                // https://www.youtube.com/watch?v=8eFeIFKAKHw - Navbar
                 echo("<header>
-                    <button>
-                        {$svg_in}
-                    </button>
+                    <nav>
+                        <input type=\"checkbox\" id=\"sidebar-active\">
+                        <label for=\"sidebar-active\" class=\"open-sidebar-button\">
+                            {$open_svg_in}
+                        </label>
+
+                        <label id=\"overlay\" for=\"sidebar-active\"></label>
+                        <div class=\"links-container\">
+                            <label for=\"sidebar-active\" class=\"close-sidebar-button\">
+                                {$close_svg_in}
+                            </label>
+
+                            <a href=\"./\">Search Page</a>
+                            <a href=\"./new.php\">Add Items</a>
+                        </div>
+                    </nav>
+
                     <h1><a href=\"./\">{$title}</a></h1>
                 </header>");
             }
